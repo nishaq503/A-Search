84ba54483ac51940b3aaeab13318f36201c3a414
Najib <najib_ishaq@my.uri.edu>
2018-04-25 18:08:11 -0400

    Whoops!

--- Board/Board.cc
+++ Board/Board.cc
@@ -37,8 +37,7 @@ bool Board::is_solvable () {
 }
 
 void Board::neighbors ( std::vector < const Board * > *neigh , char type ) {
-    int a = 5;
-    a += 5;
+    
 }
 
 unsigned int Board::hamming () {
